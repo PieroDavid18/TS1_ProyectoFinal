e078af070b1f676bb5057fe79781e87b1582d4b2
GeinerGV <geiner120300@gmail.com>
2018-11-01 15:23:31 -0500

    last

--- game/Bloques_escenario.py
+++ game/Bloques_escenario.py
@@ -1,10 +1,10 @@
 # Diseño: https://codepen.io/GeinerGV/pen/MPdMQN
-import pygame.sprite
+from pygame import sprite, Surface, Color
 
 TIPOS = {
 	"SERPIENTE": {
 		"ID": "1",
-		"COLOR": "RGB(118,238,0)",
+		"COLOR": "green",
 		"SUBS": {
 			"CABEZA": {"ID": 0},
 			"CUERPO": {"ID": 1},
@@ -14,7 +14,7 @@ TIPOS = {
 	},
 	"FONDO": {
 		"ID": 0,
-		"COLOR": "RGB(255, 255,255)"
+		"COLOR": "white"
 	}
 }
 LEVELS = {
@@ -31,28 +31,11 @@ LEVELS = {
 		"SIZ": 12
 	}
 }
-'''
-class Bloque_facil (Bloque):
-	siz = 24
-	def __init__(self):
-		super(self)
-'''
 
-class Tablero (pygame.sprite.Sprite):
-	'''
-	El área de juego
-	'''
-	def __init__(self, dimX, dimY, tipo, level):
-		if (level == LEVELS["FACIL"]["ID"]):
-			siz = 24
-		elif (level==LEVELS["MEDIO"]):
-			siz = 18
-		elif (level==LEVELS["DIFICIL"]):
-			siz = 12
-
+# Función para obtener el color del bloque de acuerdo a tipo de bloque
 def getTipoColor(tipoSelector):
 		base = TIPOS
-		color = "RGB(0, 0, 0)"
+		color = "black"
 		arrTipo = tipoSelector.split(".")
 		continuar = True
 		for idx in arrTipo:
@@ -69,16 +52,44 @@ def getTipoColor(tipoSelector):
 					except:
 						continuar = False
 					break
-			return color
+			return Color(color)
+
+'''
+class Bloque_facil (Bloque):
+	siz = 24
+	def __init__(self):
+		super(self)
+'''
+
+class Tablero (pygame.sprite.Sprite):
+	'''
+	El área de juego
+	'''
+	def __init__(self, dimX, dimY, tipo, level, serpiente):
+		if (level == LEVELS["FACIL"]["ID"]):
+			siz = 24
+		elif (level==LEVELS["MEDIO"]):
+			siz = 18
+		elif (level==LEVELS["DIFICIL"]):
+			siz = 12
+
+class AreaBloque (Bloque):
+	"""
+	Conjunto de bloques organizados
+	"""
+	def __init__(self):
+		self.
+
 
 class Bloque (pygame.sprite.Sprite):
 	'''
 	# Hacer descripción
 	'''
-	siz = 0
-	def __init__(self, posX, posY, tipo):
+	def __init__(self, pos, tipo, wid, hei):
 		self.tipo = tipo
-		self.len = len
-		self.color = getTipoColor(tipo)
-		
+		# self.color = getTipoColor(tipo)
+		self.visible = False
+		self.fragment = Surface.([wid, hei])
+		self.inner = None
+
 
